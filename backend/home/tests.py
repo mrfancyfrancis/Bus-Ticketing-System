d5affaad3439cc79e2e1c0b4272c36ddce0597f8
mrfancyfrancis <franciskyle09@yahoo.com>
2020-02-29 18:31:38 +0800

    Fixed Account Model

--- backend/home/tests.py
+++ backend/home/tests.py
@@ -1,16 +1,20 @@
 from unittest.mock import patch
+
+from django.contrib.auth.models import User
 from django.test import TestCase
 
 from home.models import PassengerAccount
 
-
-def sample_passenger_user(**kwargs):
-    return PassengerAccount.create_account(username='memeuser', email='meme@meme.com', password='password', firstname='mememan', lastname='mauricio',
-                                                age=69, birthday='1969-04-20', contact_no="09175982186")
-
+'''
+username='memeuser', email='meme@meme.com', password='password', firstname='mememan', lastname='mauricio',
+                                                age=69, birthday='1969-04-20', contact_no="09175982186"
+'''
+def sample_passenger_user(username='memeuser', email='meme@meme.com', password='password', firstname='mememan', lastname='mauricio',
+                                                age=69, birthday='1969-04-20', contact_no="09175982186"):
+    user = User.objects.create_user(username, email, password)
+    return PassengerAccount.objects.create(id=user, firstname=firstname, lastname=lastname, age=age, birthday=birthday, contact_no=contact_no)
 
 class AccountTests(TestCase):
     def test_passenger_account(self):
         acc = sample_passenger_user()
-        print(acc)
         self.assertEqual(acc.firstname, 'mememan')
